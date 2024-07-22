11c94b8da4a33fb304f891e9d7588dddb86c3eaf
John Doe <john@doe.com>
2024-07-22 12:17:16 +0200

    better process email

--- src/lib/db.js
+++ src/lib/db.js
@@ -4,6 +4,29 @@ const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
 
+const query = async (text, params) => {
+  try {
+    const result = await pool.query(text, params); // Ensure the query is awaited
+    return result;
+  } catch (error) {
+    console.error('Error making query:', text, params, error);
+    throw error;
+  }
+};
+
+const get_user_id = async (useremail) => {
+  const res = await query('SELECT id FROM users WHERE email = $1;', [useremail]);
+  if (res.rows.length === 0) {
+    throw new Error('User not found');
+  }
+  return res.rows[0].id;
+};
+
+const update_case_num = async (userid, url, casenum) => {
+  await query('UPDATE cases SET case_number = $1, status = $2 WHERE user_id = $3 AND request_url = $4;', [casenum, 'active', userid, url]);
+};
+
 module.exports = {
-  query: (text, params) => pool.query(text, params),
+  get_user_id,
+  update_case_num,
 };
--- src/lib/processEmail.js
+++ src/lib/processEmail.js
@@ -1,3 +1,4 @@
+const { get_user_id, update_case_num } = require('./db');
 const readEmails = require('./readEmail');
 const sendEmail = require('./sendEmail');
 require('dotenv').config();
@@ -10,18 +11,35 @@ async function processEmails() {
       throw new Error('Expected emails to be an array');
     }
 
+    const regex = /\[CASE (\d+)\] RESPONSE TO ([\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,})/;
+    const urlPattern = /https?:\/\/[^\s/$.?#].[^\s]*/g;
+
     for (const email of emails) {
-      const forwardedEmail = {
+      const matches = email.subject.match(regex);
+
+      if (matches) {
+        const urlmatches = email.body.match(urlPattern);
+
+        const casenum = matches[1];
+        const useremail = matches[2];
+        const urls = urlmatches[0];
+        const userid = await get_user_id(useremail);
+        await update_case_num(userid, urls, casenum);
+      }
+        
+        /*
+        const forwardedEmail = {
         to: process.env.FORWARD_TO_EMAIL, // The email address to forward the emails to
         subject: `[CASE 384983]: ${email.subject}`,
         text: `Hey, this is your respnse from google:\n\n${email.body}\n\nIf you need assistance email at nigga@niggatron.com\n`
-      };
+      }; 
 
       await sendEmail(forwardedEmail);
-      console.log('Email forwarded:', email.subject);
+      console.log('Email forwarded:', email.subject);*/
     }
   } catch (error) {
     console.error('Error processing emails:', error);
+    throw error;
   }
 }
 
